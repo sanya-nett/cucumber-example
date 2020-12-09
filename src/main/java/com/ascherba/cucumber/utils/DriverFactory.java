ebf0c745f6ff65bda5d63ecd7de15a84f35f9aa6
Aleksandr Scherba <aleksandr.scherba@iqoption.com>
2020-12-09 19:52:32 +0300

    Create web driver factory

---
+++ src/main/java/com/ascherba/cucumber/utils/DriverFactory.java
@@ -0,0 +1,17 @@
+package com.ascherba.cucumber.utils;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by aleksandr.scherba on 09.12.2020
+ */
+public class DriverFactory {
+
+    public WebDriver getDriver() {
+        ChromeDriverManager driverManager = new ChromeDriverManager();
+        driverManager.setup();
+        return new ChromeDriver();
+    }
+}
